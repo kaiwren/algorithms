7b93e8ce4c227904a4af09ad5074976ccfe32ede
Sidu Ponnappa <ckponnappa@gmail.com>
2010-07-20 19:21:50 +0530

    Added lazy natural number collection. Failing spec for a map on it.

--- src/c42/euler/problem001.clj
+++ src/c42/euler/problem001.clj
@@ -1,9 +1,13 @@
 ;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-;; Find the sum of all the multiples of 3 or 5 below 1000.
+;; Find the sum of all the multiples of 3 or 5 below 1000.10 12 15 18
 
 (ns c42.euler (:use clojure.test))
 
 (def natural-numbers (iterate inc 0))
+
+(defn natural-numbers-multiples-of-3-and-5-less-than 
+	[n]
+	n)
 
 (defn sum-multiples-less-than 
 	"Calculates the sum of all natural numbers smaller than the input that are multiples of 3 and 5" 
@@ -13,10 +17,11 @@
 (defn multiple-of?
 	"Determines if a given natural number m is a multiple of another natural number n"
 	[n m]
-	(= 0 (mod m n)))
+	(zero? (mod m n)))
 
 (testing "The sum of all natural numbers that are multiples of 3 or 5"
-	(is (= 23 (sum-multiples-less-than 10)) "smaller than 10 is 23"))
+	(is (= 23 (sum-multiples-less-than 10)) "smaller than 10")
+	(is (= 78 (sum-multiples-less-than 20)) "smaller than 20"))
 
 (testing "Multiples of"
 	(testing "3"
@@ -31,5 +36,8 @@
 		(is (not (multiple-of? 5 6)))))
 
 (testing "Natural numbers"
-	(testing "summation"
-		(is (= 1 1))))
+	(is (= (seq [0 1 2 3 4]) (take 5 natural-numbers))))
+
+(testing "filtering multiples of 3 and 5"
+	(is (= (seq [3 5 6 9]) (natural-numbers-multiples-of-3-and-5-less-than 10))))
+
